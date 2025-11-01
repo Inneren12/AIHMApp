88d9da3ff7990a5035b839dbe127f557d2742c73
Inneren12 <chulkov.oleksii.can@gmail.com>
2025-11-01 01:43:26 -0600

    Align Compose setup for app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.activity.compose)
 
     debugImplementation(libs.androidx.compose.ui.tooling)

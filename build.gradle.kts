2d3fed4ca8fbf0948cd5df27681847a050e9cd06
Inneren12 <chulkov.oleksii.can@gmail.com>
2025-11-01 00:49:12 -0400

    j

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.gradle.api.Project
+
+plugins {
+    // Reserved for future root plugins.
+}
+
+fun configureKotlinToolchain(project: Project, version: Int) {
+    project.extensions.findByName("kotlin")?.let { extension ->
+        val method = extension.javaClass.methods.firstOrNull { candidate ->
+            candidate.name == "jvmToolchain" && candidate.parameterCount == 1 &&
+                    candidate.parameterTypes.firstOrNull() == Int::class.javaPrimitiveType
+        }
+        method?.invoke(extension, version)
+    }
+}
+
+subprojects {
+    pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+        configureKotlinToolchain(this@subprojects, 17)
+    }
+
+    pluginManager.withPlugin("org.jetbrains.kotlin.android") {
+        configureKotlinToolchain(this@subprojects, 17)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs")
+    }
+}
+
+rootProject.name = "AiHandMadeApp"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
